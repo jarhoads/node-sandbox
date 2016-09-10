24b18927bf690f9b04fe0b34b570b6338d52c3bb
Josh <jarhoads@gmail.com>
2016-09-10 20:57:39 +0000

    adding functional version of date server

--- timeserverfunc.js
+++ timeserverfunc.js
@@ -2,6 +2,10 @@ var net = require("net");
 
 var port = process.argv[2];
 
+// a more functional version of the datetime server
+// I don't know if this is better, it's just something
+// I wanted to try out
+
 // a simple date and time server that 
 // takes the port as the first argument
 // and return the date in "YYYY-MM-DD hh:mm" format
@@ -13,14 +17,16 @@ var listener = function(socket){
         return (part < 10) ? "0" + part : part;
     };
     
-    var month = formatDate(date.getMonth() + 1);
-    var day = formatDate(date.getDate());
-    var hours = formatDate(date.getHours());
-    var minutes = formatDate(date.getMinutes());
+    var parts = [date.getMonth() + 1, 
+                 date.getDate(), 
+                 date.getHours(), 
+                 date.getMinutes()];
+    
+    var formattedParts = parts.map(formatDate);
     
     var dateFormat = date.getFullYear() + "-" + 
-                     month + "-" + day + " " +
-                     hours + ":" + minutes + "\n";
+                     formattedParts[0] + "-" + formattedParts[1] + " " +
+                     formattedParts[2] + ":" + formattedParts[3] + "\n";
                      
     socket.end(dateFormat);
 };
